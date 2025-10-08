d0f7ec98ea0a40dfbe2937b91115b17b31c0b722
Boburbek Jabbarganov <dearistocratpubg@gmail.com>
2025-10-08 11:58:14 +0500

    A program to find the circumference and the area of a circle

---
+++ circle.py
@@ -0,0 +1,5 @@
+r = int(input('Please enter the radius: '))
+pi = 3.14159
+area = pi*r**2
+circ = 2*pi*r
+print(f'The area of the circle is {area:.2f} cm square and the circumference is {circ:.2f} cm')
